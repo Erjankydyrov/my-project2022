c8c023ccb2f4902fdc2740e2cde94888513facdf
Erjankydyrov <erjankydyrov29@gmail.com>
2022-05-06 00:20:30 +0600

    added logo in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import classes from "./Footer.module.css";
+import Logo from "../ui/Logo/Logo";
 
 function Footer() {
   return (
     <footer className={classes.Footer}>
       <div className="container">
-        Fresh Coffee Beans 2022
+        <Logo />
+        WarGaming 2022
       </div>
     </footer>
   );
