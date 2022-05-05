c0e0686cc7eb2972d68f4e23e457d97437deff21
Erjankydyrov <erjankydyrov29@gmail.com>
2022-05-06 02:00:38 +0600

    added all images in headers and corrected styles

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,14 +1,14 @@
 import Header from "../components/Header/Header";
-import headerImage from "../assets/contacts.jpeg";
+import headerImage from "../assets/contacts.svg";
 
 function Contacts() {
   return (
     <>
-      <Header title="Order your coffee!" image={headerImage}>
-        The two most economically important varieties of coffee plant are the
-        Arabica and the Robusta; ~60% of the coffee produced worldwide is
-        Arabica and ~40% is Robusta.[5] Arabica beans consist of 0.8–1.4%
-        caffeine and Robusta beans consist of 1.7–4.0% caffeine.
+      <Header title="Order your army!" 
+        image={headerImage}
+        boxTitle="Make your choice!"
+        boxChildren="If you have any other questions, please contact support and we will definitely help you.">
+        Hobbies will help you in difficult times and help you have fun!
       </Header>
     </>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,15 +1,17 @@
 import Header from "../components/Header/Header";
-import headerImage from "../assets/products.jpeg";
+import headerImage from "../assets/products.svg";
 import ProductList from "../components/ProductList/ProductList";
 import { getProducts } from "../data/products";
 
 function Products() {
   return (
     <>
-      <Header title="Pick your type of coffee!" image={headerImage}>
-        The fruits; cherries or berries, most commonly contain two stones with
-        their flat sides together. A small percentage of cherries contain a
-        single seed, instead of the usual two. This is called a "peaberry".
+      <Header 
+        title="Take command of YOUR army and lead it into battle!" 
+        image={headerImage}
+        boxTitle="Choose your faction!"
+        boxChildren="In our assortment there are a lot of different factions and warriors who wish to come under your command.">
+        In the world of Eternal War, there are always those with whom you can fight, see which warriors will take your command.
       </Header>
 
       <ProductList products={getProducts()} />
