c1a02ff8bde7bfd41b9dde5a28fa58a148150b49
Erjankydyrov <erjankydyrov29@gmail.com>
2022-05-11 22:44:11 +0600

    added product cards

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -4,9 +4,18 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <img src={product.image} alt={product.title} />
-      <Link to={"/products/" + product.productId}>{product.title}</Link>
-      <div className={classes.price}>${product.price}</div>
+      <Link to={"/products/" + product.productId}>
+        <div className={classes.ProductItemImage}>
+          <img src={product.image} alt={product.title} />
+          <div className={classes.ProductItemText}>
+            <h3>{product.title}</h3>
+            <button>Buy</button>
+          </div>
+        </div>
+        <div className={classes.ribbon}>
+          <div className={classes.content}>{product.price}</div>
+        </div>
+      </Link>
     </div>
   );
 }
