d0a919489e6296328b7180e8edc944768712d1a5
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-22 18:19:22 +0600

    added module css in NavItem

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,9 +1,9 @@
 import { NavLink, useMatch } from "react-router-dom";
-import "./NavItem.css";
+import classes from "./NavItem.module.css"
 
 function NavItem(props) {
   return (
-    <li className="NavItem">
+    <li className={classes.NavItem}>
       <NavLink
         to={props.url}
         className={useMatch(props.url) ? classes.active : null}
