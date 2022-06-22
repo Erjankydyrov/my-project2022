5310a46ad9a482a8e1fa67f12e31eb87cc21348d
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-22 18:07:55 +0600

    added useMatch in NavItem

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,10 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useMatch } from "react-router-dom";
 import "./NavItem.css";
 
 function NavItem(props) {
   return (
     <li className="NavItem">
-      <NavLink to={props.url}>
+      <NavLink
+        to={props.url}
+        className={useMatch(props.url) ? classes.active : null}
+      >
         {props.children}
       </NavLink>
     </li>
