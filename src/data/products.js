3ece90672732c599997f86d097687812b3e91672
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-24 22:16:34 +0600

    added component smDatacards

--- src/data/products.js
+++ src/data/products.js
@@ -1,4 +1,5 @@
 import smCodex from "../assets/products/spaceMarines/smCodex/smCodex.png"
+import smDatacards from "../assets/products/spaceMarines/smDatacards/smDatacards.png";
 
 import orksCodex from "../assets/products/orks/orksCodex/orksCodex.png"
 import orksDatacards from "../assets/products/orks/orksDatacards/orksDatacards.png"
@@ -11,6 +12,14 @@ const products = [
     categoryId: "spaceMarines",
     title: "Datacards: Space Marines",
     description: `Unleash the fury of the Space Marines with these handy reference cards! Each set is great for quickly referring to your rules, making sure you'll pick the right Stratagem, Litanies of Battle or psychic powers for your battles with ease.`,
+    price: 60,
+  },
+  {
+    image: smDatacards,
+    productId: "smDatacards",
+    categoryId: "spaceMarines",
+    title: "Codex: Space Marines",
+    description: `The Space Marines are numbered amongst the most elite warriors in the Imperium. Created by the Emperor himself from the genetic material of the demigod Primarchs, they are the ultimate soldiers for a cruel and terrible age. Inside this 208-page hardback codex, you'll find everything you need to unleash the Space Marines in battle, plus thrilling lore and art to inspire your own collection.`,
     price: 60,
   },
   
