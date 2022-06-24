a36ab9d0742f412f959f67c1edd27a76def023a7
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-24 22:06:19 +0600

    added product orksDatacatrds

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,7 @@
 
 
 import orksCodex from "../assets/products/orks/orksCodex/orksCodex.png"
+import orksDatacards from "../assets/products/orks/orksDatacards/orksDatacards.png"
 
 const products = [
   
@@ -11,6 +12,14 @@ const products = [
     title: "Codex: Orks",
     description: `There are many horrific alien threats to be found among the stars, and the Orks are some of the very greatest among them. Born for battle and possessed of an all-encompassing love for war, the average Ork is a heavily muscled killing machine able to withstand tremendous punishment and heal from all but the most grievous injuries.`,
     price: 55,
+  },
+  {
+    image: orksDatacards,
+    productId: "orksDatacards",
+    categoryId: "orks",
+    title: "Orks Datacards",
+    description: `Datacards make referencing your rules in battle easy. Containing quick-reference Stratagems and psychic powers, they're perfect for ensuring you don't forget crucial rules when you need them most.`,
+    price: 29,
   },
 ];
 
