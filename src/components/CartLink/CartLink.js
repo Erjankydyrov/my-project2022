e0dcc9bdf47b088ea3fff947c12d1f37eb9756c4
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-25 00:43:15 +0600

    added styles for CartLink component

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { FaOpencart } from 'react-icons/fa';
+import classes from "./CartLink.module.css";
 
 export default function CartLink() {
   const number = useSelector((store) => {
@@ -9,5 +11,10 @@ export default function CartLink() {
     );
   });
 
-  return <NavLink to="/cart">Cart ({number})</NavLink>;
+  return (
+    <NavLink to="/cart" className={classes.CartLink}>
+      <FaOpencart className={classes.icon} />
+      <span className={classes.cart}>{number}</span>
+    </NavLink>
+  )
 }
