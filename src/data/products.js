0163446036493767205f9c7af9bedc10c389643a
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-24 21:32:44 +0600

    corrected photo name and format orksDatacards

--- src/data/products.js
+++ src/data/products.js
@@ -8,7 +8,7 @@ import SMDatacards from "../assets/products/spaceMarines/SMDatacards/SMDatacards
 import SMPrimarisEradicators from "../assets/products/spaceMarines/SMPrimarisEradicators/SMPrimarisEradicators.svg"
 
 import orksCodex from "../assets/products/orks/orksCodex/orksCodex.png"
-import OrksDatacards from "../assets/products/orks/OrksDatacards/OrksDatacards.svg"
+import orksDatacards from "../assets/products/orks/orksDatacards/orksDatacards.png"
 import WarbossinMegaArmour from "../assets/products/orks/WarbossinMegaArmour/WarbossinMegaArmour.svg"
 
 const products = [
@@ -79,16 +79,16 @@ const products = [
   },
   {
     image: orksCodex,
-    productId: "OrksCodex",
-    categoryId: "Orks",
+    productId: "orksCodex",
+    categoryId: "orks",
     title: "Codex: Orks",
     description: `There are many horrific alien threats to be found among the stars, and the Orks are some of the very greatest among them. Born for battle and possessed of an all-encompassing love for war, the average Ork is a heavily muscled killing machine able to withstand tremendous punishment and heal from all but the most grievous injuries.`,
     price: 55,
   },
   {
-    image: OrksDatacards,
-    productId: "OrksDatacards",
-    categoryId: "Orks",
+    image: orksDatacards,
+    productId: "orksDatacards",
+    categoryId: "orks",
     title: "Orks Datacards",
     description: `Datacards make referencing your rules in battle easy. Containing quick-reference Stratagems and psychic powers, they're perfect for ensuring you don't forget crucial rules when you need them most.`,
     price: 29,
@@ -96,7 +96,7 @@ const products = [
   {
     image: WarbossinMegaArmour,
     productId: "WarbossinMegaArmour",
-    categoryId: "Orks",
+    categoryId: "orks",
     title: "Warboss in Mega Armour",
     description: `Any good Ork Hunter knows the safest way to deal with a rampaging Warboss is to hit them with tank-busting artillery from as far away as possible. By donning a piston-driven suit of incredibly durable mega armour, this Warboss makes a mockery of such underhanded enemy tactics, much to the terror of his foes.`,
     price: 38,
