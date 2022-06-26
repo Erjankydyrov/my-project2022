6b7648c26f9bbb57bc44eb48b97589ae3a1a326f
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-26 14:09:41 +0600

    replaced header photo in cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
 import Header from "../components/Header/Header";
-import image from "../assets/headerImages/home.jpg";
+import image from "../assets/headerImages/cart.jpg";
 import { Link } from "react-router-dom";
 import Main from "../components/Main/Main"
 import CartDisplay from "../components/CartDisplay/CartDisplay";
@@ -9,13 +9,13 @@ function Cart() {
 
   return (
     <>
-      <Header title="Your Shopping Cart" image={image}>
+      <Header title="Your Shopping Cart" image={image} boxTitle="General gather your army." boxChildren="This is where your army gathers.">
         Please review items in your cart.
       </Header>
       <Main>
         <CartDisplay actions />
         <div className="checkoutContainer">
-          <Link to="/checkout" className="checkoutLink">CHECKOUT <BsArrowBarRight className="arrowIcon"/></Link>
+          <Link to="/checkout" className="checkoutLink">CHECKOUT <BsArrowBarRight className="arrowIcon" /></Link>
         </div>
       </Main>
     </>
