c8b4a91a02333d3628f5b093c71ce6793db9b8a0
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-27 22:32:41 +0600

    corretced some problems

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
 
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
-const key = "AIzaSyDCoBep8DiHrKiE7DsV7PIyuQjmd1swQrQ";
+const key = "AIzaSyB7YcpKNtvSPYTaM28vTrZSR9RwG8v-pTo";
 const signInUrl =
   "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
   key;
