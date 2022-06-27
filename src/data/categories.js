94bc2aab97a43f0fe2edd6543c42b422421738d4
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-27 22:14:58 +0600

    added new category in data categories

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,6 +1,7 @@
 import primaris from "../assets/categories/primaris.jpg"
 import whlogo from "../assets/whLogo.svg"
 import orcs from "../assets/categories/orks.jpg"
+import necrons from "../assets/categories/necrons.jpg"
 
 const categories = [
   {
@@ -20,6 +21,15 @@ const categories = [
     boxTitle: "Orks Is Best",
     boxChildren: "Browse the whole Orks range of brilliant units and bonkers war machines",
     description: `Waagh! We are Orcs! And we love shooters, ducks, fights and teeth! If there's any fun going on in the galaxy, we're always there!`,
+  },
+  {
+    image: necrons,
+    categoryId: "necrons",
+    title: "Necrons",
+    categoryLogo: whlogo,
+    boxTitle: "Ancient Rulers of the Galaxy",
+    boxChildren: "Arice and reclaim your long-lost empire with the Necrons.",
+    description: `The Necrons are a deathless race of alien androids, rulers of the galaxy in a long-forgotten era. Now, having slumbered in stasis for millennia, they are rising up to conquer it again.`,
   },
 ];
 
