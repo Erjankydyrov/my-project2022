b1f3347ed1f11a8f034d9b2563e722493b90989e
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-27 22:50:56 +0600

    create AuthComponent and added in Auth page

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,36 +1,14 @@
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { start } from "../redux/authSlice";
+import Main from "../components/Main/Main";
+import AuthComponent from "../components/AuthComponent/AuthComponent";
 
-export default function Auth() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  function onAuthStart(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    dispatch(start({
-      email: formData.get('email'),
-      password: formData.get('password'),
-      method: event.nativeEvent.submitter.innerText === "Sign up" ? 'signup' : 'signin',
-    }));
-
-    navigate('/');
-  }
+function Auth() {
   return (
-    <form onSubmit={onAuthStart}>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
-
-      <button>Sign in</button>
-      <button>Sign up</button>
-    </form>
+    <>
+      <Main>
+        <AuthComponent />
+      </Main>
+    </>
   );
-} 
+}
+
+export default Auth;
