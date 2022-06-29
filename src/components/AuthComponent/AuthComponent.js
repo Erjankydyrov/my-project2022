3e84603ad7c63d2ed98a8ec95b7435ad0daaffc0
Erjankydyrov <erjankydyrov29@gmail.com>
2022-06-29 22:37:08 +0600

    added styles for AuthComponent

--- src/components/AuthComponent/AuthComponent.js
+++ src/components/AuthComponent/AuthComponent.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { start } from "../../redux/authSlice";
-import classes from "./AuthComponent.module.css"
+import "./AuthComponent.css"
 
 function AuthComponent() {
   const dispatch = useDispatch();
@@ -25,19 +25,37 @@ function AuthComponent() {
     navigate("/");
   }
   return (
-    <form onSubmit={onAuthStart} className={classes.AuthComponent}>
-      <label>
-        Email:
-        <input type="email" name="email" />
+    <form onSubmit={onAuthStart} className="AuthComponent">
+    <div className="formGroup">
+      <input
+        type="email"
+        className="formField"
+        placeholder="email"
+        name="email"
+        required
+      />
+      <label htmlFor="email" className="formLabel">
+        Email
       </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
+    </div>
+    <div className="formGroup">
+      <input
+        type="password"
+        className="formField"
+        placeholder="password"
+        name="password"
+        required
+      />
+      <label htmlFor="password" className="formLabel">
+        Password
       </label>
+    </div>
 
+    <div className="btns">
       <button>Sign in</button>
       <button>Sign up</button>
-    </form>
+    </div>
+  </form>
   );
 }
 
